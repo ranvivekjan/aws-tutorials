d620d72962d55512aa952d7afc68c0ad82aac0c9
ranvivekjan <104461842+ranvivekjan@users.noreply.github.com>
2023-05-30 22:02:25 +0530

    Update server_start.sh

--- awspipelinedemo/scripts/server_start.sh
+++ awspipelinedemo/scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo chmod +x /opt/tomcat/bin/./startup.sh
-sudo /opt/tomcat/bin/./startup.sh
+sudo chmod +x /opt/tomcat/latest/bin/./startup.sh
+sudo /opt/tomcat/latest/bin/./startup.sh
